daab75e09e7067c4d65ac9eb62a521b31f810721
Zain Sajjad <zsajjad@fetchsky.com>
2019-06-19 02:07:24 +0500

    Added unsafe okhttp support in AppGlideModule

--- android/src/main/java/com/dylanvann/fastimage/FastImageGlideModule.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageGlideModule.java
@@ -1,9 +1,25 @@
 package com.dylanvann.fastimage;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
+import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.module.AppGlideModule;
+
+import java.io.InputStream;
+
+import okhttp3.OkHttpClient;
 
 // We need an AppGlideModule to be present for progress events to work.
 @GlideModule
 public final class FastImageGlideModule extends AppGlideModule {
+  @Override
+    public void registerComponents(@NonNull Context context, @NonNull Glide glide, @NonNull Registry registry) {
+        OkHttpClient okHttpClient= UnsafeOkHttpClient.getUnsafeOkHttpClient();
+        registry.append(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(okHttpClient));
+    }
 }
